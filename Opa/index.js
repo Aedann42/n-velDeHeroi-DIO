ed9717e8d6f9e87669e0af89bb7706bafd4624bb
Aedann <yuri.c.costa@hotmail.com>
2024-01-16 11:40:41 -0300

    ta pronto aehoo

---
+++ Opa/index.js
@@ -0,0 +1,20 @@
+let nomeDoHeroi = "João";
+let xpDoHeroi = 9999;
+
+if (xpDoHeroi < 1000) {
+  console.log("O nível do " + nomeDoHeroi + " é ferro");
+} else if (xpDoHeroi < 2000) {
+    console.log("O nível do " + nomeDoHeroi + " é bronze");
+} else if (xpDoHeroi < 5000) {
+    console.log("O nível do " + nomeDoHeroi + " é prata");
+} else if (xpDoHeroi < 7000) {
+    console.log("O nível do " + nomeDoHeroi + " é ouro");
+} else if (xpDoHeroi < 8000) {
+    console.log("O nível do " + nomeDoHeroi + " é platina");
+} else if (xpDoHeroi < 9000) {
+    console.log("O nível do " + nomeDoHeroi + " é ascendente");
+} else if (xpDoHeroi < 10000) {
+    console.log("O nível do " + nomeDoHeroi + " é imortal");
+} else {
+    console.log("O nível do " + nomeDoHeroi + " é radiante");   
+}
--- index.js
+++
@@ -1,1 +0,0 @@
-console.log ("SALVE RAPEIZE")
